cff73e7b4f8401e6d1f1fa9930089712fafc1945
jakobhoiras <fjz601@alumni.ku.dk>
2016-03-20 23:10:53 +0100

    added some lines

--- Frontpage.js
+++ Frontpage.js
@@ -1,45 +1,5 @@
 var current = "Frontpage";
 
-function hideDiv(theDiv) {
-	$(document).ready(function(){
-		$(theDiv).hide();
-	});
-}
-
-function showDiv(theDiv) {
-	$(document).ready(function(){
-		$(theDiv).show();
-	});
-}
-
-
-
-function createElem(type, id, className) {
-
-	if (type === 'div') {
-		var elem = document.createElement('div');
-	}
-
-	if (type === 'form') {
-		var elem = document.createElement('form');
-	}
-
-	if (type === 'inputText') {
-		var elem = document.createElement('input');
-		elem.setAttribute('type', 'text');
-	}
-	if (type === 'inputDate') {
-		var elem = document.createElement('input');
-		elem.setAttribute('type', 'date');
-	}
-
-	
-	if (id !== 0) { elem.setAttribute('id', id); }
-	if (className !== 0) { elem.setAttribute('class', className); }
-
-	return elem;
-}
-
 function goTo(target) {
 
 	var targetDiv = document.getElementById(target);
