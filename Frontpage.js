020407ccc96c05a3ddc32579d6e9d99f0fd3e57a
jakobhoiras <fjz601@alumni.ku.dk>
2016-03-21 22:00:35 +0100

    Half create goal done + box for milestones

--- Frontpage.js
+++ Frontpage.js
@@ -1,4 +1,6 @@
 var current = "Frontpage";
+var current2 = "choice1";
+var current3 = "chosenChoice1";
 
 function goTo(target) {
 
@@ -9,4 +11,21 @@ function goTo(target) {
 		currentDiv.setAttribute('class', 'FrontHide');
 
 	current = target;
+}
+function goTo2(target, target2) {
+
+	var currentDiv = document.getElementById(current2);
+		currentDiv.setAttribute('class', 'buttonChoiceOff');
+		var currentDiv2 = document.getElementById(current3);
+		currentDiv2.setAttribute('class', 'chosenChoiceOff');
+	
+
+
+	var targetDiv = document.getElementById(target);
+		targetDiv.setAttribute('class', 'buttonChoiceOn');
+		var targetDiv2 = document.getElementById(target2);
+		targetDiv2.setAttribute('class', 'chosenChoiceOn');
+
+	current2 = target;
+	current3 = target2;
 }
